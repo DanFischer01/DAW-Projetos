5e9afa8743e3ec49ed2018b09ab3d3789115897d
DanFischer01 <miojo1412@gmail.com>
2026-06-08 09:41:34 -0300

    Create index.php

---
+++ JS/alterarAV1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Daniel Fischer - AV1 - CAW: Serviço para Water Falls</title>
+</head>
+<body>
+    <h1>Sistema de perguntas e respostas</h1>
+<nav>
+    <ul>
+        <li><strong>Gestão de Utilizadores:</strong> 
+            <a href="usuarios_cadastrar.php">Cadastrar</a> | 
+            <a href="usuarios_listar.php">Listar</a>
+        </li>
+        <br>
+        <li><strong>Gestão de Perguntas:</strong>
+            <a href="perguntas_cadastrar.php?tipo=multipla">Criar Múltipla</a> | 
+            <a href="perguntas_cadastrar.php?tipo=texto">Criar Texto</a> | 
+            <a href="perguntas_listar.php">Listar Todas</a>
+        </li>
+    </ul>
+</nav>
+</body>
+</html>
