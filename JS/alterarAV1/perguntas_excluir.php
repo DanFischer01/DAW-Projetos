414312b54a3bd3ba0097369ef134740a7787d3a2
DanFischer01 <miojo1412@gmail.com>
2026-06-08 09:44:11 -0300

    Create perguntas_excluir.php

---
+++ JS/alterarAV1/perguntas_excluir.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config.php';
+$id = $_GET['id'] ?? null;
+
+if ($id) {
+    $perguntas = lerPerguntas();
+    // Filtra o array removendo quem tem o ID enviado
+    $novasPerguntas = array_filter($perguntas, function($p) use ($id) {
+        return $p['id'] != $id;
+    });
+    salvarPerguntas($novasPerguntas);
+}
+
+header("Location: perguntas_listar.php");
+exit;
